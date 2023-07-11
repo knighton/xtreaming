140d0bc8e116e25aaad1b4ae060938f6edfc0558
James Knighton <iamknighton@gmail.com>
2023-07-11 13:49:41 -0700

    Add xtensor.

---
+++ src/base/xtensor.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcomma"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wshadow-uncaptured-local"
+#pragma clang diagnostic ignored "-Wundef"
+#include "third_party/xtensor/xarray.hpp"
+#include "third_party/xtensor/xview.hpp"
+#pragma clang diagnostic pop
