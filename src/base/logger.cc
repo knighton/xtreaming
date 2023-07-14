7cf39e44b5bbdefed7342f0b418075afd20cd5b1
James Knighton <iamknighton@gmail.com>
2023-07-13 21:07:45 -0700

    Logger.

--- src/base/logger.cc
+++ src/base/logger.cc
@@ -1,5 +1,8 @@
 #include "logger.h"
 
+#include "base/string.h"
+#include "base/time.h"
+
 namespace xtreaming {
 
 bool GetLogLevel(const string& name, LogLevel* level) {
@@ -19,6 +22,52 @@ bool GetLogLevel(const string& name, LogLevel* level) {
         return false;
     }
     return true;
+}
+
+void Logger::Log(LogLevel level, const string& text) {
+    if (level < min_level_) {
+        return;
+    }
+
+    double time = (double)(NanoTime() - start_) / 1e6;
+    auto& level_name = level_names_[(size_t)level];
+    string line = StringPrintf("%14.3f [%s] %s\n", time, level_name.c_str(), text.c_str());
+    fputs(line.c_str(), file_);
+    fflush(file_);
+}
+
+bool Logger::Init(const string& path, LogLevel min_level, string* err) {
+    path_ = path;
+    file_ = fopen(path.c_str(), "ab");
+    if (!file_) {
+        return false;
+    }
+
+    min_level_ = min_level;
+    level_names_ = {
+        "",
+        "trace",
+        "debug",
+        "info",
+        "warn",
+        "error",
+        "fatal"
+    };
+    start_ = NanoTime();
+    string line = StringPrintf("[start] unix time %.9f", (double)start_ / 1e9);
+    Log(LogLevel::INFO, line);
+    return true;
+}
+
+bool Logger::Init(const string& path, const string& min_level_name, string* err) {
+    LogLevel min_level;
+    if (!GetLogLevel(min_level_name, &min_level)) {
+        *err = StringPrintf("Unknown log level (must be `trace`, `debug`, `info`, `warn`, "
+                            "`error`, or `fatal`), but got: `%s`.", min_level_name.c_str());
+        return false;
+    }
+
+    return Init(path, min_level, err);
 }
 
 }  // namespace xtreaming
--- src/base/logger.h
+++ src/base/logger.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 namespace xtreaming {
 
@@ -16,5 +18,23 @@ enum class LogLevel {
 };
 
 bool GetLogLevel(const string& name, LogLevel* level);
+
+class Logger {
+  public:
+    // Log some text at some log level.
+    void Log(LogLevel level, const string& text);
+
+    // Initialize with a file path to append to and a minimum log level.
+    bool Init(const string& path, LogLevel min_level, string* err);
+    bool Init(const string& path, const string& min_level, string* err);
+
+  private:
+    string path_;                 // Where to log to.
+    FILE* file_;                  // File handle.
+    LogLevel min_level_;          // Minimum logging level.
+    vector<string> level_names_;  // Mapping of log level enum to string name.
+    int64_t start_;               // Logger start time in nanoseconds since the epoch
+                                  // (everything thereafter notes milliseconds since start).
+};
 
 }  // namespace xtreaming
